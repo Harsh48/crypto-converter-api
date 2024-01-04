4af4045ce31a69dd45e63dc66f5c5a3819b3526a
Harsh Raj <harsh.techilia@gmail.com>
2024-01-04 19:22:35 +0530

    feat: create getFiatList controller

--- controllers/crypto.controller.js
+++ controllers/crypto.controller.js
@@ -1,4 +1,25 @@
-const getFiatList = async (req, res) => {};
+const getFiatList = async (req, res) => {
+	try {
+		const response = await axios.get(
+			"https://pro-api.coinmarketcap.com/v1/fiat/map",
+			{
+				params: {
+					limit: 100,
+					sort: "id",
+				},
+				headers: {
+					"X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API_KEY,
+				},
+			}
+		);
+
+		res.json({
+			data: response.data,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 const getCryptoList = async (req, res) => {};
 
