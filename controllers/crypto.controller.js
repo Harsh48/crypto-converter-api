dbcfdc450b902fb0779d36b86540e09854561c1c
Harsh Raj <harsh.techilia@gmail.com>
2024-01-04 19:21:43 +0530

    feat: configure routes and controllers

---
+++ controllers/crypto.controller.js
@@ -0,0 +1,11 @@
+const getFiatList = async (req, res) => {};
+
+const getCryptoList = async (req, res) => {};
+
+const convertCryptoToFiat = async (req, res) => {};
+
+module.exports = {
+	getFiatList,
+	getCryptoList,
+	convertCryptoToFiat,
+};
---
+++ routes/crypto.route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const cryptoController = require("../controllers/crypto.controller");
+
+router.get("/fiat/list", cryptoController.getFiatList);
+
+router.get("/crypto/list", cryptoController.getCryptoList);
+
+router.get("/convert", cryptoController.convertCryptoToFiat);
+
+module.exports = router;
